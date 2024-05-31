0d0d13c9c43d006f24baf77e51fc6fef1d65fe9c
mrdapoyo <wiiuforever25@gmail.com>
2024-05-31 21:28:46 +0200

    Changes

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,14 +1,12 @@
-let raw = "https://weirdscifi.ratiosemper.com/neocities.php?sitename=dapoyo"
+const url = "https://weirdscifi.ratiosemper.com/neocities.php?sitename=dapoyo";
 
-// Parse the JSON string
-const data = JSON.parse(raw);
-
-// Get the <p> tags
-const siteName = document.getElementById("site-name");
-const createdAt = document.getElementById("created-at");
-const updatedAt = document.getElementById("updated-at");
-
-// Set the text content of the <p> tags
-siteName.textContent = data.info.sitename;
-createdAt.textContent = data.info.created_at;
-updatedAt.textContent = data.info.last_updated;
+fetch(url)
+ .then(response => response.json())
+ .then(data => {
+    console.log(data);
+    const createdAt = document.getElementById("created-at");
+    const updatedAt = document.getElementById("updated-at");
+    createdAt.innerHTML = data.info.created_at;
+    updatedAt.innerHTML = data.info.last_updated;
+  })
+ .catch(error => console.error("Error:", error));
